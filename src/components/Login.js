87764d1ea91e853eda3cb5320941401f6f9e0d4d
amohata007 <amohata.007@gmail.com>
2024-08-17 16:57:41 +0530

    Form Validations

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,20 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
+import { Validation } from "../utils/Validation";
 
 const Login = () => {
   const [isSignIn, setIsSignIn] = useState(true);
+  const [displayErrMessage, setDisplayErrMessage] = useState(null);
+
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const checkValidation = () => {
+    const errMessage = Validation(email.current.value, password.current.value);
+    console.log(errMessage);
+    setDisplayErrMessage(errMessage);
+  };
+
   const toggleSignIn = () => {
     setIsSignIn(!isSignIn);
   };
@@ -15,7 +27,10 @@ const Login = () => {
           alt="BG_Image"
         ></img>
       </div>
-      <form className="rounded-lg bg-opacity-70 p-10 w-3/12 absolute bg-black text-white my-36 mx-auto left-0 right-0">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="rounded-lg bg-opacity-70 p-10 w-3/12 absolute bg-black text-white my-36 mx-auto left-0 right-0"
+      >
         <h1 className="text-3xl m-2 my-4 font-bold">
           {isSignIn ? "Sign In" : "Sign Up"}
         </h1>
@@ -27,16 +42,22 @@ const Login = () => {
           ></input>
         )}
         <input
+          ref={email}
           type="email"
           placeholder="Enter your Email"
           className="p-2 my-4 mx-2 w-full ml-1 bg-gray-700"
         ></input>
         <input
+          ref={password}
           type="password"
           placeholder="Enter your Password"
           className="p-2 my-4 mx-2 w-full ml-1 bg-gray-700"
         ></input>
-        <button className="p-2 mx-2 my-8 ml-1 w-full bg-red-700 rounded-lg">
+        <p className="py-2 font-bold text-red-500">{displayErrMessage}</p>
+        <button
+          className="p-2 mx-2 my-8 ml-1 w-full bg-red-700 rounded-lg"
+          onClick={checkValidation}
+        >
           {isSignIn ? "Sign In" : "Sign Up"}
         </button>
         <p className="p-2 cursor-pointer" onClick={toggleSignIn}>
---
+++ src/utils/Validation.js
@@ -0,0 +1,16 @@
+export const Validation = (email, password) => {
+  const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  const isPasswordValid =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@.#$!%*?&^])[A-Za-z\d@.#$!%*?&]{8,15}$/.test(
+      password
+    );
+
+  if (!isEmailValid) {
+    return "Email is not valid";
+  }
+  if (!isPasswordValid) {
+    return "Password is not valid";
+  }
+
+  return null;
+};
